22fe110b4f18333e45bb45e4e18413b2e30441f7
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-04-07 20:15:57 -0700

    Added prompts for lowercase and uppercase

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ generateBtn.addEventListener("click", writePassword);
 
 passwordLength = () => {
   let passLength = window.prompt("Enter a password length between 8 and 128:");
+  // Confirm number between below range
   if(passLength >= 8 && passLength <= 128) {
     return passLength
+  // Alert that there is an invalid entry and reprompt for length again
   } else {
     window.alert("Invalid entry");
     passwordLength();
@@ -24,6 +26,32 @@ passwordLength = () => {
 
 }
 
+includeLowercase= () => {
+  let lowercase = window.prompt("Should the password include lowercase letters? Yes or No?")
+  // Confirm a yes or no response
+  if(["yes", "no"].includes(lowercase.toLowerCase())) {
+    return true;
+  } else {
+    // Alert there is an invalid entry and reprompt
+    window.alert("Invalid entry");
+    includeLowercase();
+  }
+}
+
+includeUppercase= () => {
+  let uppercase = window.prompt("Should the password include uppercase letters? Yes or No?")
+  // Confirm a yes or no response
+  if(["yes", "no"].includes(uppercase.toLowerCase())) {
+    return true;
+  } else {
+    // Alert there is an invalid entry and reprompt
+    window.alert("Invalid entry");
+    includeUppercase();
+  }
+}
+
 generatePassword = () => {
-  passwordLength()
+  passLength = passwordLength();
+  lowercase = includeLowercase();
+  uppercase = includeUppercase();
 }
